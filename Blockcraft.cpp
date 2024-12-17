8aca830277545498ec923c3f759a220de9142f64
Miles Royer <130029923+MilesRoyerr@users.noreply.github.com>
2024-12-17 15:12:36 -0600

    Update Blockcraft.cpp

--- Blockcraft.cpp
+++ Blockcraft.cpp
@@ -38,9 +38,9 @@ int main() {
     }
 
   
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // Use OpenGL 3.x
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // Use core profile
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); 
 
 
     GLFWwindow* window = glfwCreateWindow(800, 600, "Blockcraft - OpenGL Window", NULL, NULL);
