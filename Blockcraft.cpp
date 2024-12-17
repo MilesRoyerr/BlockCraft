cba8bd4e64ffc7e1125e30cc481445bf344a5ed7
Miles Royer <130029923+MilesRoyerr@users.noreply.github.com>
2024-12-17 15:13:45 -0600

    Update Blockcraft.cpp

--- Blockcraft.cpp
+++ Blockcraft.cpp
@@ -76,7 +76,7 @@ int main() {
             glfwSetWindowShouldClose(window, true);
         }
 
-        // Clear the color and depth buffer
+       
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         drawCube();
